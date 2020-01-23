1fdd3418f1674efb520818e2591fcbb2af1e7407
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2020-01-23 11:35:01 +0800

    public inferface to this library

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,10 @@ use std::fmt;
 use syn::parenthesized;
 
 use syn::parse::Result as SynResult;
+
+#[proc_macro_derive(Builder, attributes(builder))]
+pub fn builder_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).expect("Could not parse type to derive Builder for");
+
+    impl_builder_macro(ast)
+}
